15f6e60506e93d8220adc814bb9a5af94fbb5850
Shivam Kejriwal <shivamkejriwal1@gmail.com>
2022-01-24 18:32:20 +0530

    chore: split settings.py file

---
+++ real_estate/settings/development.py
@@ -0,0 +1,14 @@
+from .base import *
+
+
+DATABASES = {
+    "default": {
+        "ENGINE": env("POSTGRES_ENGINE"),
+        "NAME": env("POSTGRES_DB"),
+        "USER": env("POSTGRES_USER"),
+        "PASSWORD": env("POSTGRES_PASSWORD"),
+        "HOST": env("PG_HOST"),
+        "PORT": env("PG_PORT"),
+    }
+}
+
